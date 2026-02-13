0211b483446cd0d38a28d24f4f343c5c1101d278
Solmayer Alves <solmayer.alves@gmail.com>
2026-02-13 16:00:21 -0300

    Primeiro commit

---
+++ src/test/java/com/solmayer/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.solmayer.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
