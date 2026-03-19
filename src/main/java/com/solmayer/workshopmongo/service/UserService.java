d3158719d6f492ee78be21709353fd4c3a455029
Solmayer Alves <solmayer.alves@gmail.com>
2026-03-19 17:09:37 -0300

    Deleção de usuário com DELETE

--- src/main/java/com/solmayer/workshopmongo/service/UserService.java
+++ src/main/java/com/solmayer/workshopmongo/service/UserService.java
@@ -29,6 +29,11 @@ public class UserService {
 		return repository.save(obj);
 	}
 	
+	public void delete(String id) {
+		findById(id);
+		repository.deleteById(id);
+	}
+	
 	public User fromDto(UserDTO objDto) {
 		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
