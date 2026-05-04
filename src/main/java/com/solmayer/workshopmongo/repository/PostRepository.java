8a2d01edea6c572819d26f6ac1378a88664da3df
Solmayer Alves <solmayer.alves@gmail.com>
2026-05-04 17:26:02 -0300

    Consulta com vários critérios

--- src/main/java/com/solmayer/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/solmayer/workshopmongo/repository/PostRepository.java
@@ -1,5 +1,6 @@
 package com.solmayer.workshopmongo.repository;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
@@ -13,4 +14,7 @@ public interface PostRepository extends MongoRepository<Post, String>{
 	List<Post> searchTitle(String text);
 	
 	List<Post> findByTitleContainingIgnoreCase(String text);
+	
+	@Query("{ 'date': { $gte: ?1, $lte: ?2 }, $or: [ { 'title': { $regex: ?0, $options: 'i' } }, { 'body': { $regex: ?0, $options: 'i' } }, { 'comments.text': { $regex: ?0, $options: 'i' } } ] }")
+	List<Post> fullSearch(String text, Date minDate, Date maxDate);
 }
